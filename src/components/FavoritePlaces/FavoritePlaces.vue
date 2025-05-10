66a0fda1d73e9a26312d50e96607673cf4d2d8ba
Ilya Oriekhov <164690908+IlyaOriekhov@users.noreply.github.com>
2025-05-10 21:08:09 +0300

    component status

--- src/components/FavoritePlaces/FavoritePlaces.vue
+++ src/components/FavoritePlaces/FavoritePlaces.vue
@@ -1,11 +1,19 @@
 <script setup>
 import FavoritePlace from '../FavoritePlace/FavoritePlace.vue'
 import IButton from '../IButton/IButton.vue'
+import { ref, reactive } from 'vue'
+
+const buttonVariant = ref('gradient')
+
+const changeButtonVariant = () => {
+  buttonVariant.value = buttonVariant.value === 'gradient' ? 'outlined' : 'gradient'
+}
 </script>
 
 <template>
   <div class="px-6">
     <div class="text-[#939393] mb-4">Додані маркери</div>
+
     <!-- <FavoritePlace v-for="n in 4" :key="n" /> -->
     <!-- <IButton class="w-full mt-1і">Додати маркер</IButton> -->
     <slot name="label"></slot>
@@ -14,6 +22,8 @@ import IButton from '../IButton/IButton.vue'
     </slot>
 
     <slot></slot>
-    <IButton class="w-full mt-10">Додати маркер</IButton>
+    <IButton class="w-full mt-10" :variant="buttonVariant" @click="changeButtonVariant"
+      >Додати маркер</IButton
+    >
   </div>
 </template>
