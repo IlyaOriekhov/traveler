b811c881c901312277b7dc2b8a391c12db7da43b
Ilya Oriekhov <164690908+IlyaOriekhov@users.noreply.github.com>
2025-05-10 18:29:54 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#F3743D',
+      },
+    },
+  },
+  plugins: [],
+}
