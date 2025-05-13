ac8f587f1d8a2c965f52b270d64dd1bf9f750f38
Ilya Oriekhov <164690908+IlyaOriekhov@users.noreply.github.com>
2025-05-13 23:10:56 +0300

    crud logout

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,7 @@
 <script setup>
-// import HomepageView from './views/HomepageView.vue'
-// import GreetingView from './views/GreetingView.vue'
-// import AuthView from './views/AuthView.vue'
 import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <RouterView />
-  <!-- <GreetingView />
-  <HomepageView />
-  <AuthView /> -->
+  <router-view />
 </template>
